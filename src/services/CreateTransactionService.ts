57185411bfc847facb74e302dc3553bf859fd224
Arnxld <pedro.arnold13@gmail.com>
2020-10-01 17:26:05 -0300

    desafio 06 finalized

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,11 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository, getRepository } from 'typeorm';
 
-import { getCustomRepository } from 'typeorm'
+import AppError from '../errors/AppError';
 
-import TransactionsRepository from '../repositories/TransactionsRepository'
+import TransactionsRepository from '../repositories/TransactionsRepository';
 
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
 
 interface Request {
   title: string;
@@ -14,18 +15,49 @@ interface Request {
 }
 
 class CreateTransactionService {
-  public async execute({title, value, type, category}: Request): Promise<Transaction> {
-    const transactionsRepository = getCustomRepository(TransactionsRepository)
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+    const categoryRepository = getRepository(Category);
 
+    // verificar se o balanço está suficiente para criar a transação
+    const { total } = await transactionsRepository.getBalance();
+
+    if (type === 'outcome' && total < value) {
+      throw new AppError('You do not have enough balance');
+    }
+
+    // verificar se a categoria já existe
+    let transactionCategory = await categoryRepository.findOne({
+      where: {
+        title: category,
+      },
+    });
+
+    // não existe? criar ela
+    if (!transactionCategory) {
+      transactionCategory = categoryRepository.create({
+        title: category,
+      });
+
+      await categoryRepository.save(transactionCategory);
+    }
+
+    // existe? então buscar ela do banco de dados e usar o id retorna
     const transaction = transactionsRepository.create({
       title,
       value,
-      type
-    })
+      type,
+      category: transactionCategory,
+    });
 
-    await transactionsRepository.save(transaction)
+    await transactionsRepository.save(transaction);
 
-    return transaction
+    return transaction;
   }
 }
 
